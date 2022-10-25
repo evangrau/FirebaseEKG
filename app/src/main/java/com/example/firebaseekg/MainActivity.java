f97be1cecc4bffcd827ac108df327f2c7825d4a1
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-10-25 13:54:09 -0500

    MainActivity mostly done, started on ChatMessage model

--- app/src/main/java/com/example/firebaseekg/MainActivity.java
+++ app/src/main/java/com/example/firebaseekg/MainActivity.java
@@ -2,13 +2,75 @@ package com.example.firebaseekg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button registerButton, loginButton, logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerButton = (Button) findViewById(R.id.btn_register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton = (Button) findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutButton = (Button) findViewById(R.id.btn_logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOut();
+            }
+        });
+
+        // setting all of the buttons to invisible to start
+        registerButton.setVisibility(View.GONE);
+        loginButton.setVisibility(View.GONE);
+        logoutButton.setVisibility(View.GONE);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null)
+        {
+            registerButton.setVisibility(View.VISIBLE);
+            loginButton.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            logoutButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void logOut() {
+        // logs out the user
+        FirebaseAuth.getInstance().signOut();
+
+        // recreates the activity so that the user sees the original screen
+        recreate();
     }
 }
